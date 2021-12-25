cc85c83f897e01ee174a8e12662c33defb303191
zTags <wierdgeomery@gmail.com>
2021-12-25 20:28:52 +0100

    window (?)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 fn main() {
-    assert!(9 + 10 ==  21);
+    let sdl_context = sdl2::init().unwrap();
+    let video_context = sdl_context.video().unwrap();
+    let window = video_context.window("ReBLCK", 1280, 720).build().unwrap();
 }
 

d87a60b9e60ffd30285ad62b74bdc28e73aae96d
zTags <wierdgeomery@gmail.com>
2021-12-26 11:49:30 +0100

    readme

--- README.md
+++ README.md
@@ -4,6 +4,13 @@ A revival and reimplemention of [BLCK](https://github.com/FlowVix/BLCK) in Rust.
 ## building
 you need
 - cargo stable
-- the setup for (fltk-rs)[https://github.com/Rust-SDL2/rust-sdl2]
+- [sdl2](https://libsdl.org/download)
+- [sdl2\_gfx](https://sourceforge.net/projects/sdl2gfx/)
+
+if you're on linux you can just
+```sh
+$ pacman -S sdl2 sdl2_gfx
+```
+
 ## Warning
 **RΞBLCK is in very early development and currently is not functional.**
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,38 @@
+use sdl2::event::Event;
+use sdl2::pixels::Color;
+use sdl2::gfx::framerate::FPSManager;
+
+use std::process::exit;
+
+const FPS: u32 = 60;
+
 fn main() {
     let sdl_context = sdl2::init().unwrap();
-    let video_context = sdl_context.video().unwrap();
-    let window = video_context.window("ReBLCK", 1280, 720).build().unwrap();
+    let video = sdl_context.video().unwrap();
+    let window = video.window("ReBLCK", 1280, 720).position_centered().build().unwrap();
+    let mut fps_manager = FPSManager::new();
+    let mut canvas = window.into_canvas().build().unwrap();
+    
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    
+    canvas.set_draw_color(Color::RGB(50, 50, 50));
+    fps_manager.set_framerate(FPS);
+
+    'running: loop {
+        // handle events
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} => break 'running,
+                _ => {}    
+            }
+        }
+        println!("{}", fps_manager.get_frame_count());
+        // draw
+        canvas.clear(); // todo change this
+        canvas.present();
+        // fps
+    }
+    // handle shutdown
+    exit(0);
 }
 
